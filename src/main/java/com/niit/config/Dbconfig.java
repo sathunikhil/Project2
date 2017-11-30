59559ffcd53a1acb19da51bc6f14374e01526cca
Nikhil <Nikhil@DESKTOP-OG17K2H>
2017-11-30 14:53:12 +0530

    project2

---
+++ src/main/java/com/niit/config/Dbconfig.java
@@ -0,0 +1,86 @@
+package com.niit.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+import com.niit.dao.BlogDAO;
+import com.niit.dao.BlogDAOImpl;
+import com.niit.dao.ForumDAO;
+import com.niit.dao.ForumDAOImpl;
+import com.niit.model.Blog;
+import com.niit.model.Forum;
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan("com.social")
+@Component
+public class Dbconfig 
+{	
+	@Bean(name = "dataSource")
+	public DataSource getOracleDataSource()
+	{
+		DriverManagerDataSource driverManagerDataSource=new DriverManagerDataSource();
+		driverManagerDataSource.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+		driverManagerDataSource.setUrl("jdbc:oracle:thin:@localhost:1521:XE");
+		driverManagerDataSource.setUsername("niit1");
+		driverManagerDataSource.setPassword("niit1");
+		return driverManagerDataSource;
+	}
+	public Properties getHibernateProperties()
+	{
+		Properties properties=new Properties();
+		properties.setProperty("hibernate.hbm2ddl.auto", "update");
+		properties.put("hibernate.dialect","org.hibernate.dialect.OracleDialect");
+		return properties;
+	}
+	
+	@Autowired
+	@Bean(name = "sessionFactory")
+	public SessionFactory getSessionFactory()
+	{
+		LocalSessionFactoryBuilder localSessionFactoryBuilder=new LocalSessionFactoryBuilder(getOracleDataSource());
+		localSessionFactoryBuilder.addProperties(getHibernateProperties());
+		localSessionFactoryBuilder.addAnnotatedClass(Blog.class);
+		localSessionFactoryBuilder.addAnnotatedClass(Forum.class);
+		localSessionFactoryBuilder.scanPackages("com.social");
+		System.out.println("SessionFactory Bean Created");
+		return localSessionFactoryBuilder.buildSessionFactory();
+	}
+	
+	@Autowired
+	@Bean(name = "transactionManager")
+	public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) 
+	{
+		HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
+		System.out.println("Transaction");
+		return transactionManager;
+	}
+	
+	@Autowired
+	@Bean(name = "blogDAO")
+	public BlogDAO getBlogDAO(SessionFactory sessionFactory)
+	{
+		System.out.println("Blog DAO object Created");
+		return new BlogDAOImpl(sessionFactory);
+	}
+	@Autowired
+	@Bean(name = "forumDAO")
+	public ForumDAO getForumDAO(SessionFactory sessionFactory)
+	{
+		System.out.println("Forum object Created");
+		return new ForumDAOImpl(sessionFactory);
+	}
+}
---
+++ src/main/java/com/niit/model/Forum.java
@@ -0,0 +1,62 @@
+package com.niit.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Forum 
+{
+	//private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue
+	int forumId;
+	String forumName;
+	String forumContent;
+	int UserId;
+	String status;
+	Date createDate;
+	public int getForumId() {
+		return forumId;
+	}
+	public void setForumId(int forumId) {
+		this.forumId = forumId;
+	}
+	public String getForumName() {
+		return forumName;
+	}
+	public void setForumName(String forumName) {
+		this.forumName = forumName;
+	}
+	public String getForumContent() {
+		return forumContent;
+	}
+	public void setForumContent(String forumContent) {
+		this.forumContent = forumContent;
+	}
+	public int getUserId() {
+		return UserId;
+	}
+	public void setUserId(int userId) {
+		UserId = userId;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public Date getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	
+
+}
